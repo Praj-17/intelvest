791473b24e2743980bab8903de2a7d18ab58ad6c
prajwal <pwaykos1@gmail.com>
2024-10-05 22:35:23 +0530

    added okama reporter

--- src/modules/okama_reporter.py
+++ src/modules/okama_reporter.py
@@ -1,5 +1,18 @@
 
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import json
+
 class OkamaReporter:
     def __init__(self) -> None:
-        pass
+        self.attributes = None
+        with open(os.getenv("okama_attributes_static_file"), "r") as f:
+            self.attributes = json.load(f)
+    def get_attributes(self):
+        return self.attributes
+    def get_an_attribute(self, portfolio_obj, attribute):
+        return portfolio_obj.__getattribute__(attribute)
     
+
+
