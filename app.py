cc0c74aaa3c373b89f52832f29e5c3682f346027
prajwal <pwaykos1@gmail.com>
2024-10-06 11:00:25 +0530

    added home page

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 # app/main.py
 from fastapi import FastAPI
 from src.routes import user_router, portfolio_router, analysis_router
-
+from starlette.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from fastapi.staticfiles import StaticFiles
 app = FastAPI(
     title="Intelvest",
     description="Server for Intelvest",
@@ -11,3 +14,12 @@ app = FastAPI(
 app.include_router(user_router)
 app.include_router(portfolio_router)
 app.include_router(analysis_router)
+
+# Mount static directory for serving CSS, JS, and images
+app.mount("/assets", StaticFiles(directory="src/assets"), name="assets")
+# Set up Jinja2Templates to render HTML templates
+templates = Jinja2Templates(directory=r"src\assets")
+# Serve the homepage
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("home_page.html", {"request": request})
